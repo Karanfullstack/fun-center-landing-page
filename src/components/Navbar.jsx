38a19f45021e389a1d43b4c5af523d7ed81b6808
Karan Chauhan <karanchouhan211@gmail.com>
2025-07-17 15:00:51 +0200

    add navbar responsive fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import logo from "../assets/Vector.svg";
+
 export default function Navbar() {
     return (
         <header>
-            <nav className="w-full font-hubot h-[88px] flex items-center justify-between px-5  bg-black">
-                <div className="flex items-center w-[180px] h-[40px]  md:w-[218.61px] md:h-[48px] px-2 ">
-                    <img src={logo} alt="logo" />
+            <nav className="w-full font-hubot h-[88px] flex items-center justify-between px-4 md:px-6 bg-black">
+                <div className="flex items-center h-10 md:h-12">
+                    <img
+                        src={logo}
+                        alt="logo"
+                        className="h-full w-auto max-w-[140px] md:max-w-[220px]"
+                    />
                 </div>
-                <div>
-                    <button className="bg-[#DBFD01] py-[8px]  text-[16px] uppercase leading-[100%] font-bold font-hubot font-semicondensed md:w-[153px] md:h-[48px] h-[40px] w-[140px]  min-w-[120px] fill-transparent">
+
+                <div className="flex-shrink-0">
+                    <button className="bg-[#DBFD01] text-black text-sm md:text-base uppercase font-bold font-hubot font-semicondensed py-2 px-3  w-full md:w-[153px] h-10 md:h-12 overflow-hidden truncate">
                         pobierz apkę
                     </button>
                 </div>
