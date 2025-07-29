43085685a649d29657236d06a21422b7511f6151
Karan Chauhan <karanchouhan211@gmail.com>
2025-07-29 15:44:06 +0200

    add smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Lenis from "lenis";
 import Download from "./components/Download";
 import Footer from "./components/Footer";
 import Frame from "./components/Frame";
@@ -6,16 +7,36 @@ import Hero from "./components/Hero";
 import Ranking from "./components/Ranking";
 import Slides from "./components/Slides";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 function App() {
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t) => t,
+            smoothWheel: true,
+            smoothTouch: true,
+            touchMultiplier: 2,
+        });
+
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
     const page2Ref = useRef(null);
 
     const scrollToPage2 = () => {
         page2Ref.current?.scrollIntoView({ behavior: "smooth" });
     };
     return (
-        <div className="h-screen snap-y scroll-container snap-mandatory  w-screen m-auto overflow-y-scroll  scroll-smooth scrollbar-hide ">
+        <div className=" h-screen snap-y   relative  scroll-container snap-mandatory  max-w-[1440px] m-auto overflow-y-scroll  scroll-smooth scrollbar-hide ">
             <Hero onScroll={scrollToPage2} />
 
             <Frame />
